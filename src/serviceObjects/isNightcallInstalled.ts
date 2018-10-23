b13c7055472268e1cd4831f3a636471fa988c9a8
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-10-23 10:47:21 +0200

    add types in isNightcallInstalled

--- src/serviceObjects/isNightcallInstalled.ts
+++ src/serviceObjects/isNightcallInstalled.ts
@@ -1,8 +1,8 @@
 import { homedir } from "os";
 import { access, constants } from "fs";
 
-export function isNightcallInstalled() {
-  return new Promise((resolve, reject) => {
+export function isNightcallInstalled():Promise<boolean> {
+  return new Promise((resolve) => {
     access(`${homedir()}/.nightcall`, constants.F_OK, err => {
       if (err) {
         resolve(false);
