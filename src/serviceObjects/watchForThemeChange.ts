3bb16c05f12b42fb48484f0a08c0452c949868bc
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-10-23 10:45:52 +0200

    add checking current theme on startup

--- src/serviceObjects/watchForThemeChange.ts
+++ src/serviceObjects/watchForThemeChange.ts
@@ -8,20 +8,25 @@ import {
   WATCHFILE_CONTENTS_NIGHT
 } from "../util/Constants";
 
-export function watchForThemeChange() {
-  const path = `${homedir()}/.nightcall`;
-  watch(path, { persistent: false }, (event, filename) => {
-    readFile(path, "utf8", (err: NodeJS.ErrnoException, contents: string) => {
-      if (err === null && contents.length > 0) {
-        switch (contents) {
-          case WATCHFILE_CONTENTS_DAY:
-            changeTheme(DAY_THEME);
-            break;
-          case WATCHFILE_CONTENTS_NIGHT:
-            changeTheme(NIGHT_THEME);
-            break;
-        }
+function updateTheme(path: string): void {
+  readFile(path, "utf8", (err: NodeJS.ErrnoException, contents: string) => {
+    if (err === null && contents.length > 0) {
+      switch (contents) {
+        case WATCHFILE_CONTENTS_DAY:
+          changeTheme(DAY_THEME);
+          break;
+        case WATCHFILE_CONTENTS_NIGHT:
+          changeTheme(NIGHT_THEME);
+          break;
       }
-    });
+    }
   });
+}
+
+export function watchForThemeChange(): void {
+  const path = `${homedir()}/.nightcall`;
+  watch(path, { persistent: false }, () => {
+    updateTheme(path);
+  });
+  updateTheme(path);
 }
